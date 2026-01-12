e8943c14873a2e2587eb0b3adb3c8cb43078ce91
cash-eze2 <amoscash14@gmail.com>
2026-01-12 11:44:23 +0000

    views

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('/', views.user_login, name='login'),
     path('login/', views.user_login, name='login'),
+    path('admin_dashbord', views.user, name='admin_dashboard'),
+    path('teacher_dashboard', views.user, name='teacher'),
+    path('student_dashboard', views.user, name='student'),
 ]
